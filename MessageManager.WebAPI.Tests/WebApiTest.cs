66349d600a2a6dde7bc4e2affcac00dc89f1c2a7
yuezhongxin <yuezhongxin@gmail.com>
2014-06-02 21:56:23 +0800

    MessageManager.WebAPI.Tests添加及修改

---
+++ MessageManager.WebAPI.Tests/WebApiTest.cs
@@ -0,0 +1,38 @@
+﻿/**
+* author:xishuai
+* address:https://www.github.com/yuezhongxin/MessageManager
+**/
+
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageManager.WebAPI.Tests
+{
+    [TestFixture]
+    public class WebApiTest
+    {
+        [Test]
+        public static void MessageWebAPI()
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:9283/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+
+                var message = await client.GetAsync("api/News/GetAllNews");
+            }
+        }
+    }
+}
