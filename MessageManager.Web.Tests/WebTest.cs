7894a6d8060f7e31b5b8dcf58ab2c627387bbfb8
yuezhongxin <yuezhongxin@gmail.com>
2014-06-03 22:50:24 +0800

    MessageManager.Web.Tests添加Web单元测试

---
+++ MessageManager.Web.Tests/WebTest.cs
@@ -0,0 +1,22 @@
+﻿/**
+* author:xishuai
+* address:https://www.github.com/yuezhongxin/MessageManager
+**/
+
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MessageManager.Web.Tests
+{
+    [TestFixture]
+    public class WebTest
+    {
+        [Test]
+        public void MessageWeb()
+        {
+        }
+    }
+}
