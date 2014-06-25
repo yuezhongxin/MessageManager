f121fd71bf37dd6b1f9a42c6dcd58fc6ad9cae36
yuezhongxin <yuzhongxin@gmail.com>
2014-06-25 17:13:07 +0800

    MessageManager.Domain.Demo 文件添加

---
+++ MessageManager.Domain.Demo/DomainModel/User.cs
@@ -0,0 +1,35 @@
+﻿/**
+* author:xishuai
+* address:https://www.github.com/yuezhongxin/MessageManager
+**/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MessageManager.Domain.Demo.DomainModel
+{
+    public class User : IAggregateRoot
+    {
+        #region 构造方法
+        public User()
+        {
+            this.ID = Guid.NewGuid().ToString();
+        }
+        #endregion
+        
+        #region 实体成员
+        public string Name { get; set; }
+        public virtual ICollection<Message> SendMessages { get; set; }
+        public virtual ICollection<Message> ReceiveMessages { get; set; }
+        #endregion
+
+        #region IEntity成员
+        /// <summary>
+        /// 获取或设置当前实体对象的全局唯一标识。
+        /// </summary>
+        public string ID { get; set; }
+        #endregion
+    }
+}
---
+++ MessageManager.Domain.Demo/IAggregateRoot.cs
@@ -0,0 +1,12 @@
+﻿/**
+* author:xishuai
+* address:https://www.github.com/yuezhongxin/MessageManager
+**/
+
+namespace MessageManager.Domain.Demo
+{
+    /// <summary>
+    /// 表示继承于该接口的类型是聚合根类型。
+    /// </summary>
+    public interface IAggregateRoot : IEntity { }
+}
