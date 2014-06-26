d2fa43e39f5445833ce060c58246a88b432cac86
yuezhongxin <yuezhongxin@qq.com>
2014-06-26 22:17:31 +0800

    MessageManager.Domain.Demo.Tests DomainDemo单元测试添加

---
+++ MessageManager.Domain.Demo.Tests/DomainTest.cs
@@ -0,0 +1,31 @@
+﻿using MessageManager.Domain.Demo.V1;
+using MessageManager.Domain.Demo.V1.Event;
+using Microsoft.Practices.Unity;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MessageManager.Domain.Demo.Tests
+{
+    [TestFixture]
+    public class DomainTest
+    {
+        [Test]
+        public void MessageTest()
+        {
+            IUnityContainer _container;
+
+            _container = new UnityContainer()
+                        .RegisterType<IDomainEvent, MessageEvent>()
+                        .RegisterType<IHandle<Message>, MessageEventHandler>("MessageEventHandler");
+
+            DomainEvents.Container = _container;
+            User testSendUser = new User { Name = "小菜" };
+            User testReceiveUser = new User { Name = "小菜" };
+            Message message = new Message { Title = "test", Content = "test", ToUser = testSendUser };
+            testSendUser.SendMessage(message);
+        }
+    }
+}
