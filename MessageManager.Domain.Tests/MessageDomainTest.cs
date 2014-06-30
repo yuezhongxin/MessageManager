f448a753ae8217f43ed02c5a41f527033963fbf0
yuezhongxin <yuezhongxin@qq.com>
2014-06-30 23:16:54 +0800

    MessageManager.Domain.Tests nuint改为xunit及其他调整

---
+++ MessageManager.Domain.Tests/MessageDomainTest.cs
@@ -0,0 +1,32 @@
+﻿/**
+* author:xishuai
+* address:https://www.github.com/yuezhongxin/MessageManager
+**/
+
+using MessageManager.Domain.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace MessageManager.Domain.Tests
+{
+    public class MessageDomainTest
+    {
+        [Fact]
+        public void SendMessageTest()
+        {
+            User sendUser = new User("小菜");
+            User receiveUser = new User("大神");
+            Message message = new Message("test", "test", sendUser);
+            Assert.Equal(message.Send(receiveUser),true);
+        }
+
+        [Fact]
+        public Message ReadMessageTest()
+        {
+            return null;
+        }
+    }
+}
