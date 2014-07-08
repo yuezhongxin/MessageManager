cd4a696b92bc939d3caf76ceac4a7e10e3217ff5
yuezhongxin <yuezhongxin@gmail.com>
2014-07-08 09:43:46 +0800

    MessageManager.Domain/DomainService/VerifyMessageService.cs 更新

--- MessageManager.Domain/DomainService/VerifyMessageService.cs
+++ MessageManager.Domain/DomainService/VerifyMessageService.cs
@@ -17,9 +17,13 @@ namespace MessageManager.Domain.DomainService
         public static OperationResponse<Message> VerifyMessage(Message message)
         {
             //示例业务规则，对象导航关联访问需要探讨
-            if (message.SendUser.SendMessages.Where(m => m.SendTime.Date == DateTime.Now.Date).Count() > 100)
+            if (message.SendUser.SendMessages.Where(m => m.SendTime.Date == DateTime.Now.Date).Count() > 200)
             {
-                return new OperationResponse<Message>(false, "发件人一天之内只能发送一百个短消息");
+                return new OperationResponse<Message>(false, "发件人一天之内只能发送200个短消息");
+            }
+            if (message.SendUser.SendMessages.Where(m => m.SendTime.Date == DateTime.Now.Date && m.ReceiveUser == message.ReceiveUser).Count() > 50)
+            {
+                return new OperationResponse<Message>(false, "发件人一天之内只能和同一人发送50个短消息");
             }
             //to do...
             return new OperationResponse<Message>(true, "发送消息成功", message);
