76fb46b9180c04b9ef63ffcd9a04536d4b2ec53d
yuezhongxin <yuezhongxin@gmail.com>
2014-07-08 11:02:14 +0800

    MessageManager.Domain.Tests/MessageDomainTest.cs 更新

--- MessageManager.Domain.Tests/MessageDomainTest.cs
+++ MessageManager.Domain.Tests/MessageDomainTest.cs
@@ -3,7 +3,9 @@
 * address:https://www.github.com/yuezhongxin/MessageManager
 **/
 
+using MessageManager.Domain.DomainService;
 using MessageManager.Domain.Entity;
+using MessageManager.Infrastructure;
 using Xunit;
 
 namespace MessageManager.Domain.Tests
@@ -13,16 +15,29 @@ namespace MessageManager.Domain.Tests
         [Fact]
         public void DomainTest_SendMessage()
         {
-            //User sendUser = new User("xiaocai", "小菜");
-            //Message message = new Message("test", "test", sendUser);
-            //User receiveUser = new User("dashen", "大神");
-            //Assert.True(message.Send(receiveUser).IsSuccess);
+            User sendUser = new User("xiaocai", "小菜");
+            User receiveUser = new User("dashen", "大神");
+            Message message = new Message("test", "test", sendUser, receiveUser);
+            OperationResponse<Message> serviceResult = VerifyMessageService.VerifyMessage(message);
+            Assert.True(VerifyMessageService.VerifyMessage(message).IsSuccess);
         }
 
         [Fact]
-        public Message DomainTest_ReadMessage()
+        public void DomainTest_ReadMessage()
         {
-            return null;
+            User readUser = new User("xiaocai", "小菜");
+            Message message = new Message("test", "test", new User("xiaocai", "小菜"), new User("dashen", "大神"));
+            OperationResponse<Message> serviceResult = ReadMessageService.ReadMessage(message, readUser);
+            Assert.True(ReadMessageService.ReadMessage(message, readUser).IsSuccess);
+        }
+
+        [Fact]
+        public void DomainTest_ReplyMessage()
+        {
+            User replyUser = new User("dashen", "大神");
+            Message message = new Message("test", "test", new User("xiaocai", "小菜"), new User("dashen", "大神"));
+            OperationResponse<Message> serviceResult = VerifyMessageService.VerifyMessage(message);
+            Assert.True(VerifyMessageService.VerifyMessage(message).IsSuccess);
         }
     }
 }
